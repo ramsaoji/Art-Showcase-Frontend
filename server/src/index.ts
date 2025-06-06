4572572bb81b09818a5ae23d9d8063e86cda43bc
Ram Saoji <mygithub@techness.com>
2025-06-06 17:35:20 +0530

    refactor: remove Firebase configuration and migration scripts, transitioning to tRPC and Prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,43 +1,45 @@
 // src/index.ts
-import app from './server';
-import config from './config';
-import logger from './config/logger';
+import app from "./server";
+import config from "./config";
+import logger from "./config/logger";
 
 // Start the server
 const server = app.listen(config.port, () => {
   logger.info(
     { port: config.port, env: config.env },
-    `🚀 Server started on port ${config.port} in ${config.env} mode`
+    `Server started on port ${config.port} in ${config.env} mode`
   );
 });
 
 // Handle graceful shutdown
 const gracefulShutdown = (signal: string) => {
-  logger.info({ signal }, 'Received shutdown signal');
-  
+  logger.info({ signal }, "Received shutdown signal");
+
   server.close(() => {
-    logger.info('HTTP server closed');
+    logger.info("HTTP server closed");
     process.exit(0);
   });
-  
+
   // Force close after timeout
   setTimeout(() => {
-    logger.error('Could not close connections in time, forcefully shutting down');
+    logger.error(
+      "Could not close connections in time, forcefully shutting down"
+    );
     process.exit(1);
   }, 10000);
 };
 
 // Listen for termination signals
-process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
-process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
+process.on("SIGINT", () => gracefulShutdown("SIGINT"));
 
 // Handle uncaught exceptions and rejections
-process.on('uncaughtException', (error) => {
-  logger.fatal({ error }, 'Uncaught exception');
+process.on("uncaughtException", (error) => {
+  logger.fatal({ error }, "Uncaught exception");
   process.exit(1);
 });
 
-process.on('unhandledRejection', (reason) => {
-  logger.fatal({ reason }, 'Unhandled rejection');
+process.on("unhandledRejection", (reason) => {
+  logger.fatal({ reason }, "Unhandled rejection");
   process.exit(1);
 });
